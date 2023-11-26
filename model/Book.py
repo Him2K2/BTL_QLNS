076c0f8a5dc61b06faa7095a2e692c6f260cc8b0
Hồ Xuân Vũ <131353334+Him2K2@users.noreply.github.com>
2023-11-27 00:14:49 +0700

    Update Book.py

--- model/Book.py
+++ model/Book.py
@@ -1,4 +1,4 @@
-import BookDocument
+from model.BookDocument import BookDocument
 class Book(BookDocument):
     def __init__(self, id, publisher, inventory, quantitySale, price, author, pages):
         super().__init__(id, publisher, inventory, quantitySale, price)
@@ -12,3 +12,13 @@ class Book(BookDocument):
     #getter & setter of pages
     def get_Page(self): return self.__pages
     def set_Page(self, value): self.__pages = value
+    def show_Book(self):
+        print("-" * 30)
+        print(f"Mã sách: {self.get_Id()}")
+        print(f"Nhà xuất bản: {self.get_Publisher()}")
+        print(f"Số lượng tồn kho: {self.get_Quantity()}")
+        print(f"Số lượng bán theo ngày: {self.get_Sold_Per_Day()}")
+        print(f"Giá tiền: {self.get_Price()}")
+        print(f"Tác giả: {self.get_Author()}")
+        print(f"Số trang: {self.get_Num_Page()}")
+        print("-" * 30)
