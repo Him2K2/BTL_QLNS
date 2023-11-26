b9690ea008c0a5f19d37607af4be15e32888bf7c
Hồ Xuân Vũ <131353334+Him2K2@users.noreply.github.com>
2023-11-27 00:17:20 +0700

    Update NewsPaper.py

--- model/NewsPaper.py
+++ model/NewsPaper.py
@@ -1,4 +1,5 @@
-import BookDocument
+from model.BookDocument import BookDocument
+
 class Book(BookDocument):
     def __init__(self, id, publisher, inventory, quantitySale, price, releaseDate):
         super().__init__(id, publisher, inventory, quantitySale, price)
@@ -7,3 +8,8 @@ class Book(BookDocument):
     #getter & setter of release date
     def get_Release_Date(self): return self.__releaseDate
     def set_Release_Date(self, value): self.__releaseDate = value
+    def showNewPaper(self):
+        super().display()
+        print(f"Ngày phát hành: {self.__publishDate}")
+
+
